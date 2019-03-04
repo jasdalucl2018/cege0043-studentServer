065770766358ae7b9ef285d87ebce205951a8524
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-03-04 09:33:25 -0500

    19-3-4 adding test.html to studentserver.js file

--- studentServer.js
+++ studentServer.js
@@ -1,34 +1,36 @@
-// express is the server that forms part of the nodejs program 
-
+// express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
-var app = express();
+var app = express(); // set up app to run server using express API
+
 // add an http server to serve files to the Edge browser
-// due to certificate issues it rejects the https files if they are not // directly called in a typed URL
+// due to certificate issues it rejects the https files if they are not
+// directly called in a typed URL
+
 var http = require('http');
-var httpServer = http.createServer(app); 
+var httpServer = http.createServer(app); // create server with the app
 httpServer.listen(4480);
 
-// 19_2_19 12:03 adding functionality to return test.html when called
-
-app.get('/test.html', function (req, res){
-	// run serverside code
-	console.log ('test.html requested');
-	// note that "_dirname" means diretory name and gives path to studentserver.js file
-	re.sendFile(_dirname + '/test.html');
+// serving text
+app.get('/', function(req, res){
+	// server-side code
+	console.log("The server has received a request.");
+	res.send("hello world from the HTTP server");
 });
 
-app.get('/',function (req,res) {
-	res.send("hello world from the HTTP server"); 
+// serving content
+app.get('/test.html', function(req, res){
+	// server-side code
+	console.log('test.html requested');
+	// __dirname gives the path to the studentServer.js file
+	res.sendFile(__dirname + '/test.html');
 });
 
 
-
-// adding functionality to log the requests 
-app.use(function (req, res, next) {
+// add functionality to log requests
+app.use(function(req, res, next){
 	var filename = path.basename(req.url);
-	var extension = path.extname(filename); 
-	console.log("The file " + filename + " was requested."); next();
+	var extension = path.extname(filename);
+	console.log("The file " + filename + " was requested.");
+	next();
 });
-
-
