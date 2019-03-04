d21bf88c9fc4871dc61e5e647606e0efb9074aaa
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-03-04 09:47:06 -0500

    19-4-3 debugging studentserver.js file

--- studentServer.js
+++ studentServer.js
@@ -1,36 +1,33 @@
 // express is the server that forms part of the nodejs program
 var express = require('express');
-var path = require("path");
-var app = express(); // set up app to run server using express API
+var path = require('path');
+var app = express();
 
 // add an http server to serve files to the Edge browser
-// due to certificate issues it rejects the https files if they are not
-// directly called in a typed URL
-
+// due to certificate issues it rejects the https files
+// if they are not directly called in a typed URL
 var http = require('http');
-var httpServer = http.createServer(app); // create server with the app
+var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
-// serving text
-app.get('/', function(req, res){
-	// server-side code
-	console.log("The server has received a request.");
-	res.send("hello world from the HTTP server");
+
+app.get('/', function (req, res) {
+    res.send("hello world from the HTTP server");
 });
 
-// serving content
-app.get('/test.html', function(req, res){
-	// server-side code
-	console.log('test.html requested');
-	// __dirname gives the path to the studentServer.js file
-	res.sendFile(__dirname + '/test.html');
+// Modify the code so that the server returns test.html
+// when it is requested
+app.get('/test.html', function (req, res) {
+    // run some server-side code
+    console.log('test.html requested');
+    // note that __dirname gives the path to the studentServer.js file
+    res.sendFile(__dirname + '/test.html');
 });
 
-
-// add functionality to log requests
-app.use(function(req, res, next){
-	var filename = path.basename(req.url);
-	var extension = path.extname(filename);
-	console.log("The file " + filename + " was requested.");
-	next();
+// adding functionality to log the requests
+app.use(function (req, res, next) {
+    var filename = path.basename(req.url);
+    var extension = path.extname(filename);
+    console.log("The file " + filename + " was requested.");
+    next();
 });
